90ba42fd30483d0554579be88c17d39e8afd36bd
Jiří Čechák <j.cechak1@gmail.com>
2019-04-13 22:17:05 +0200

    added build

---
+++ src/src/pa181/UI/build/precache-manifest.45e4448423c9110f22e709c60d1c7001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "453d7313356c80229128",
+    "url": "/static/js/main.8a58c388.chunk.js"
+  },
+  {
+    "revision": "102353b65c86c76ca582",
+    "url": "/static/js/2.76bd369c.chunk.js"
+  },
+  {
+    "revision": "453d7313356c80229128",
+    "url": "/static/css/main.e4229f4d.chunk.css"
+  },
+  {
+    "revision": "37e0c40df8c554495732a9a1322a3a70",
+    "url": "/index.html"
+  }
+];
