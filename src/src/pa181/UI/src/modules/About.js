dee3a9f6cd6920f7c5c5a728f9649b4450c3b8eb
Jiří Čechák <j.cechak1@gmail.com>
2019-04-13 15:32:48 +0200

    updated UI about

--- src/src/pa181/UI/src/modules/About.js
+++ src/src/pa181/UI/src/modules/About.js
@@ -1,11 +1,72 @@
 import React from "react";
 
 const About = ({ texts }) => (
-  <div {...{ className: "flex-col-centered" }}>
+  <div {...{ className: "flex-col-centered width-full" }}>
     <h1>{texts.ABOUT}</h1>
-    <p {...{ className: "margin-none" }}>
-      Adrián Tóth, Tadeáš Pavlík, Jiří Čechák, Václav Stehlík, Jan Ondruch
-    </p>
+    <div {...{ className: "flex-col-centered width-full" }}>
+      <h3
+        {...{
+          className: "margin-bottom-small-only normal text-center width-full"
+        }}
+      >
+        {texts.TITLE}:
+      </h3>
+      <h3 {...{ className: "margin-bottom-only text-center width-full" }}>
+        {texts.TERM_PROJECT_FOR_COURSE_PA181}
+      </h3>
+    </div>
+    <div {...{ className: "flex-col-centered width-full" }}>
+      <h3
+        {...{
+          className: "margin-bottom-small-only normal text-center width-full"
+        }}
+      >
+        {texts.AUTHORS}:
+      </h3>
+      <h3 {...{ className: "margin-bottom-only text-center width-full" }}>
+        Adrián Tóth, Tadeáš Pavlík, Jiří Čechák, Václav Stehlík, Jan Ondruch
+      </h3>
+    </div>
+    <div {...{ className: "flex-col-centered width-full" }}>
+      <h3
+        {...{
+          className: "margin-bottom-small-only normal text-center width-full"
+        }}
+      >
+        {texts.DESCRIPTION}:
+      </h3>
+      <h3 {...{ className: "margin-bottom-only text-center width-full" }}>
+        {texts.APP_DESCRIPTION}
+      </h3>
+    </div>
+    <div {...{ className: "flex-col-centered width-full" }}>
+      <h3
+        {...{
+          className: "margin-bottom-small-only normal text-center width-full"
+        }}
+      >
+        {texts.SOURCE}:
+      </h3>
+      <h3 {...{ className: "margin-bottom-only text-center width-full" }}>
+        <a href="https://github.com/europ/MUNI-FI-PA181">
+          https://github.com/europ/MUNI-FI-PA181
+        </a>
+      </h3>
+    </div>
+    <div {...{ className: "flex-col-centered width-full" }}>
+      <h3
+        {...{
+          className: "margin-bottom-small-only normal text-center width-full"
+        }}
+      >
+        {texts.LICENSE}:
+      </h3>
+      <h3 {...{ className: "margin-bottom-only text-center width-full" }}>
+        <a href="https://github.com/europ/MUNI-FI-PA181/blob/master/LICENSE">
+          https://github.com/europ/MUNI-FI-PA181/blob/master/LICENSE
+        </a>
+      </h3>
+    </div>
   </div>
 );
 
