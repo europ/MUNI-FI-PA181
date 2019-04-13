6d1a262fbc1ed01d5906632da68d1da6eba45a13
Václav Stehlík <vasastehlik@email.cz>
2019-04-13 12:28:07 +0200

    Add frontend assets to wwwroot

---
+++ src/src/API/wwwroot/precache-manifest.abcccc662880a55a29a881ccea67327f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c8dfa310bcd7d618fa68",
+    "url": "/static/js/main.a4466d04.chunk.js"
+  },
+  {
+    "revision": "102353b65c86c76ca582",
+    "url": "/static/js/2.76bd369c.chunk.js"
+  },
+  {
+    "revision": "c8dfa310bcd7d618fa68",
+    "url": "/static/css/main.2880acc1.chunk.css"
+  },
+  {
+    "revision": "262386366245b82af73edde3fb80e22a",
+    "url": "/index.html"
+  }
+];
