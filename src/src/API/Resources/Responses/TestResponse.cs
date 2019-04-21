5360e6b826520dd001f99603b8c46f7667fe105e
Václav Stehlík <vasastehlik@email.cz>
2019-04-21 11:33:13 +0200

    Update TestResponse
    Change type of 'Language' from CultureInfo to string

--- src/src/API/Resources/Responses/TestResponse.cs
+++ src/src/API/Resources/Responses/TestResponse.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Globalization;
 
 namespace API.Resources.Responses
 {
@@ -8,6 +7,6 @@ namespace API.Resources.Responses
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public CultureInfo Language { get; set; }
+        public string Language { get; set; }
     }
 }
