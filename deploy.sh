4098205160ef39a60c42a5b62b3d1751d6ca043e
Adrián Tóth <adricko28@gmail.com>
2019-04-26 12:59:58 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-# ENVIROMENT PROPERTIES
-# TOKEN - github personal access token - 'repo:status' only
-# REPO - string in format 'RepoOwner/RepoName'
 
-if [ -z "${TOKEN}" ] || [ -z "${REPO}" ]; then
-  echo -e "\033[1;31mMissing environment properties (\$REPO or \$TOKEN)\!\033[0m"
+# ENVIROMENT PROPERTIES
+# TOKEN - github personal access token
+# REPO - The Git repository name - string in format 'RepoOwner/RepoName'
+# GIT_COMMIT - The Git commit - SHA
+
+
+# USAGE: error "message"
+error() {
+  echo -e "\033[1;31m${1}\033[0m"
   exit 1
-fi
+}
+
+# USAGE: create_status "status"
+create_status() {
+  echo -e "\033[1;33mCOMMIT STATUS=${1}\033[0m"
+
+  curl \
+  --silent \
+  --show-error \
+  --header "Authorization: token ${TOKEN}" \
+  --data "{\"state\": \"${1}\", \"context\": \"IBM Cloud\", \"description\": \"Deployment\", \"target_url\": \"http://cloud.ibm.com/\"}" \
+  --url "https://api.github.com/repos/${REPO}/statuses/${GIT_COMMIT}" \
+  --output /dev/null
+
+  echo -e "\033[0;33mCOMMIT STATUS '${1}' SET\033[0m"
+}
+
+
+[[ -z "${TOKEN}" ]] && error "Missing environment property 'TOKEN'\!"
+[[ -z "${REPO}" ]] && error "Missing environment property 'REPO'\!"
+[[ -z "${GIT_COMMIT}" ]] && error "Missing environment property 'GIT_COMMIT'\!"
 
 cd src # directory including source files
 
-SHA=$(git rev-parse HEAD) # commit hash (latest commit)
-
-STATUS="pending"
-echo -e "\033[1;33mSTATUS=$STATUS\033[0m"
-
-curl \
-  --header "Authorization: token ${TOKEN}" \
-  --url "https://api.github.com/repos/${REPO}/statuses/${SHA}" \
-  --data "{\"state\": \"${STATUS}\", \"context\": \"IBM Cloud\", \"description\": \"Deployment\", \"target_url\": \"http://cloud.ibm.com/\"}" \
-  --output /dev/null
-
+create_status "pending"
 cf push "${CF_APP}"
-if [ $? -eq 0 ]; then export STATUS="success"; else export STATUS="error"; fi
-echo -e "\033[1;33mSTATUS=$STATUS\033[0m"
+if [ $? -eq 0 ]; then create_status "success"; else create_status "error"; fi
 
-curl \
-  --header "Authorization: token ${TOKEN}" \
-  --url "https://api.github.com/repos/${REPO}/statuses/${SHA}" \
-  --data "{\"state\": \"${STATUS}\", \"context\": \"IBM Cloud\", \"description\": \"Deployment\", \"target_url\": \"http://cloud.ibm.com/\"}" \
-  --output /dev/null
+exit 0
