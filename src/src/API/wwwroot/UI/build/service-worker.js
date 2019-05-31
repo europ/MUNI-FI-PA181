c2a5dd5abbc3cf6002e4e04e48cb73f09e5c0dad
Jiří Čechák <j.cechak1@gmail.com>
2019-05-31 11:45:32 +0200

    ui build

--- src/src/API/wwwroot/UI/build/service-worker.js
+++ src/src/API/wwwroot/UI/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72474f00958e2fae85ccc5c789ad918e.js"
+  "/precache-manifest.7de55cdefbef85fa4d9bf37b9fa39185.js"
 );
 
 workbox.clientsClaim();
